19df97af38369ff6988e694063380ce71dd500ac
Srajan <srivasrajan8@gmail.com>
2025-06-04 23:32:15 +0530

    Runtime Polymorphism

--- OOPS/Polymorphism.cpp
+++ OOPS/Polymorphism.cpp
@@ -1,24 +1,63 @@
 #include <iostream>
 using namespace std;
 
-class Param{
+// Runtime Polymorpism
+// Overriding
+class Animal{
     public:
-        int val;
-        void operator+(Param& o2){
-            int val1=this->val;
-            int val2=o2.val;
-            cout<<val2-val1<<endl;  
-        }
+    virtual void speak(){
+        cout<<"Speaking"<<endl;
+    }
+};
+class Dog:public Animal{
+    public:
+    // override
+    void speak(){
+        cout<<"Bark"<<endl;
+    }
 };
 
-// Operator Overloading
+// overriding main
 int main(){
-    Param o1,o2;
-    o1.val=10;
-    o2.val=3;
-    o1+o2;
-    return 0;
+
+    // Dynamic Allocation
+    // Animal* a = new Animal();
+    // a->speak();
+
+    // Dog* a = new Dog();
+    // a->speak();
+
+    // Upcasting
+    Animal* a= new Dog();
+    a->speak();
+
+    // DownCast
+    Dog* b = (Dog* )new Animal();
+    b->speak();
+    // Static allocation
+    // Dog d;
+    // d.speak();
 }
+
+// // Operator overloading
+// class Param{
+//     public:
+//         int val;
+//         void operator+(Param& o2){
+//             int val1=this->val;
+//             int val2=o2.val;
+//             cout<<val2-val1<<endl;  
+//         }
+// };
+
+// // Operator Overloading
+// int main(){
+//     Param o1,o2;
+//     o1.val=10;
+//     o2.val=3;
+//     o1+o2;
+//     return 0;
+// }
 // Function Overloading
 // class Maths{
 //     public:
