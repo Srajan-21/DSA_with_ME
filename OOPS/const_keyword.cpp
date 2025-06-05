759e55f3efa944a2f46c1eb5cc7aecc8df821f9b
Srajan <srivasrajan8@gmail.com>
2025-06-06 00:52:53 +0530

    const pointer

---
+++ OOPS/const_keyword.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // const int x = 5; //x is constant
+    // // initialisation can be done
+    // // but we cant re-assign a value
+    // // x=10; //error
+    // cout<<x<<endl;
+
+    // 2.const with pointer
+    // const int *a = new int (2); // const data no const pointer
+    // if we write const before * then content will be constant
+    // int const *a = new int(2);
+    // cout<<*a<<endl;
+    // // *a=20; //error cant change content of pointer
+    // int b=20;
+    // a=&b; // pointer itself can be reassigned
+    // cout<<*a<<endl;
+
+    // 3. const poinyter non constant data
+    // int *const a = new int (2);
+    // cout<<*a<<endl;
+    // *a=20; //no error
+    // cout<<*a<<endl;
+    // int b=40;
+    // // a=&b; //error pointer constant
+
+    // 4. const pointer const data
+    // const int *const a = new int(10);
+    // cout<<*a<<endl;
+    // *a=20; //error
+    // int b=100;
+    // a=&b; //error
+
+}
