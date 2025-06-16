7bdfca53b1ef78ecea8ed1c438717d06d8244042
Srajan <srivasrajan8@gmail.com>
2025-06-17 04:51:03 +0530

    Deletion at Position in Linked List

--- LINKED LIST/Deletion.cpp
+++ LINKED LIST/Deletion.cpp
@@ -18,10 +18,6 @@ class Node{
     }
 
     ~Node(){
-        if (this->next != NULL) {
-            delete next;
-            this->next = NULL;
-        }
         cout << "Memory freed for node with data " << this->data << endl;
     }
 };
@@ -60,9 +56,18 @@ void deletionAtHead(Node* &head)
     delete temp;
 }
 
-void deletionAtTail(Node* & head , Node* tail)
+void deletionAtTail(Node* &head , Node* &tail)
 {
     int len = length(head);
+
+    // if one node
+    if(head==tail)
+    {
+        delete tail;
+        head = NULL;
+        tail = NULL;
+        return;
+    }
 
     int i = 1;
     Node* prev = head;
@@ -78,34 +83,90 @@ void deletionAtTail(Node* & head , Node* tail)
     delete temp;
 }
 
+void deletionAtPosition(Node* &head , Node* &tail , int position)
+{
+    int len = length(head);
+
+
+    if(position == 0)
+    {
+        deletionAtHead(head);
+        return;
+    }
+
+    if(position>=len)
+    {
+        deletionAtTail(head,tail);
+        return;    
+    }
+
+    int i = 1;
+    Node* prev = head;
+    while(i<position-1)
+    {
+        prev = prev->next;
+        i++;
+    }
+
+    Node* curr = prev->next;
+
+    prev->next = curr->next;
+    curr->next = NULL;
+    delete curr;
+}
+
 int main(){
     Node* first = new Node(1);
     Node* second = new Node(2);
     Node* third = new Node(3);
     Node* fourth = new Node(4);
     Node* fifth = new Node(5);
+    Node* sixth = new Node(6);
+    Node* seventh = new Node(7);
+    Node* eighth = new Node(8);
+
+    Node* head = first;
+    Node* tail = eighth;
 
     first->next = second;
     second->next = third;
     third->next = fourth;
     fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = seventh;
+    seventh->next = eighth; 
 
     cout<<"Printing Linked List before Deletion"<<endl;
-    print(first);
+    print(head);
 
     cout<<endl;
     cout<<endl;
 
-    deletionAtHead(first);
+    deletionAtHead(head);
     cout<<"After Deletion"<<endl;
-    print(first);
-
+    print(head);
+    cout<<endl;
+    cout<<"Head:"<<head->data<<endl;
+    cout<<"Tail:"<<tail->data<<endl;
     cout<<endl;
     cout<<endl;
 
-    deletionAtTail(first , fifth);
+    deletionAtTail(head , tail);
     cout<<"After Deletion"<<endl;
-    print(first);
+    print(head);
+    cout<<endl;
+    cout<<"Head:"<<head->data<<endl;
+    cout<<"Tail:"<<tail->data<<endl;
 
+    cout<<endl;
+    cout<<endl;
+
+    deletionAtPosition(head , tail , 3);
+    cout<<"After Deletion"<<endl;
+    print(head);
+    cout<<endl;
+    cout<<"Head:"<<head->data<<endl;
+    cout<<"Tail:"<<tail->data<<endl;
+    
     return 0;
 }
