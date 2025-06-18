5eaf9c4a8752593eb5428db82b1ebe6be88fc07b
Srajan <srivasrajan8@gmail.com>
2025-06-18 21:03:34 +0530

    Leetcode 237 Delete Node in a Linked List where head is not given

---
+++ LINKED LIST/leet237_DeleteNode.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node * next;
+
+    Node(){
+        this->data = 0;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node* &head)
+{
+    Node* temp = head;
+    while (temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+void deleteNode(Node* node) {
+        node->data = node->next->data;      
+        node->next = node->next->next;
+    }
+
+int main(){
+    Node* first = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+    Node* fourth = new Node(4);
+    Node* fifth = new Node(5);
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+
+    cout<<"Printing Linked List"<<endl;
+    print(first);
+
+    deleteNode(fourth);
+    cout<<endl;
+    cout<<"Printing Linked List"<<endl;
+    print(first);
+
+}
