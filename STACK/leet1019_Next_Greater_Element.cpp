144df24c00be10eef28a7f49059d03db8dff83f8
Srajan <srivasrajan8@gmail.com>
2025-07-11 04:29:11 +0530

    Leetcode 71 Simplify Path

---
+++ STACK/leet1019_Next_Greater_Element.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(){
+        this->data = 0;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+vector<int> nextLarge(Node* head){
+    vector<int>l;
+    while(head){
+        l.push_back(head->data);
+        head = head->next;
+    }
+
+    stack<int>st;
+    vector<int>ans(l.size());
+
+    for(int i=0;i<l.size();i++){
+        while(!st.empty() && l[i]>l[st.top()]){
+            int k=st.top();
+            st.pop();
+            ans[k]=l[i];
+        }
+        st.push(i);
+    }
+    return ans;
+}
+
+int main(){
+    Node* first = new Node(2);
+    Node* second = new Node(7);
+    Node* third = new Node(4);
+    Node* fourth = new Node(3);
+    Node* fifth = new Node(5);
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    
+    vector<int> ans = nextLarge(first);
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    
+    return 0;
+}
