8f99ddd3ed29024fe3b15e605c13e314ebc5d9a4
Srajan <srivasrajan8@gmail.com>
2025-07-15 02:43:29 +0530

    Leetcode 394 Decode String

---
+++ STACK/leet394_Decode_String.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+string decode(string s){
+    stack<string> st;
+    for(auto ch:s){
+        if(ch == ']')
+        {
+            string sToRepeat = "";
+            while(!st.empty() && st.top()!="["){
+                string top = st.top();
+                sToRepeat += top;
+                st.pop();
+            }
+            st.pop();
+
+            string numTimes = "";
+            while(!st.empty() && isdigit(st.top()[0])){
+                numTimes += st.top();
+                st.pop();
+            }
+
+            reverse(numTimes.begin() , numTimes.end());
+            int n = stoi(numTimes);
+
+            string currentDecode = "";
+            while(n--){
+                currentDecode += sToRepeat;
+            }
+            st.push(currentDecode);
+        }
+        else {
+            string temp(1,ch);
+            st.push(temp);
+        }
+    }
+
+    string ans;
+    while(!st.empty()){
+        ans+=st.top();
+        st.pop();
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
