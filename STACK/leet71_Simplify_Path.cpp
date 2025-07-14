0dab132ac0035bfdd8d480955219578488916df7
Srajan <srivasrajan8@gmail.com>
2025-07-15 00:04:50 +0530

    Leetcode 71 Simplify Path

---
+++ STACK/leet71_Simplify_Path.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void buildAns(stack<string>&s,string& ans){
+    if(s.empty()){
+        return;
+    }
+    string minPath = s.top(); 
+    s.pop();
+    buildAns(s,ans);
+    ans+="/"+minPath;
+}
+
+string simplify(string &path){
+    stack<string>s;
+    int i=0;
+
+    while(i<path.size()){
+        int start=i;
+        int end=i+1;
+        while(end<path.size()&&path[end]!='/'){
+            ++end;
+        }
+        string minPath = path.substr(start,end-start);
+        i=end;
+
+        if(minPath=="/"||minPath=="/."){
+            continue;
+        }
+        if(minPath!="..")s.push(minPath);
+        else if (!s.empty())s.pop();       
+    }
+
+    string ans=s.empty()?"/":"";
+    buildAns(s,ans);
+    return ans;
+}
+
+int main(){
+    string s = "/home/user/Documents/../Pictures";
+    string ans = simplify(s);
+    cout<<ans;
+    // cout<<longest(s);
+}
