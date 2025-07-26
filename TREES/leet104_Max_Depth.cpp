7dd40541b1a6dd4651240b1cad4ff526dc0cd681
Srajan <srivasrajan8@gmail.com>
2025-07-27 02:34:21 +0530

    Leetcode 104 Maximum Depth of Binary Tree

---
+++ TREES/leet104_Max_Depth.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+Node* buildTree(){
+
+    int data;
+    cout<<"Enter the data : "<<endl;
+    cin>>data;
+
+    if(data == -1)
+        return NULL;
+
+    Node* root = new Node(data);
+
+    cout<<"Enter data for Left of "<<data<<" Node"<<endl;
+    root->left = buildTree();
+    
+    cout<<"Enter data for right of "<<data<<" Node"<<endl;
+    root->right = buildTree();
+
+    return root;
+}
+
+void levelOrderTraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+ 
+        if(temp == NULL){
+            // last level traversed completely
+            cout<<endl;
+            if(!q.empty()){
+                // q still has some child nodes
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+
+int height(Node* root){
+    // Base Case
+    if(root == NULL){
+        return 0;
+    }
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    int ans = max(left , right) + 1;
+    return ans;
+}
+
+int main(){
+    Node* root = buildTree();
+    cout<<"Root : "<<root->data<<endl<<endl;
+    // 3 9 -1 -1 20 15 -1 -1 7 -1 -1
+
+    cout<<endl<<endl;
+    
+    cout<<"Level order : "<<endl;
+    levelOrderTraversal(root);
+    cout<<endl<<endl;
+
+    cout<<"Height : "<<height(root);
+    return 0;
+}
