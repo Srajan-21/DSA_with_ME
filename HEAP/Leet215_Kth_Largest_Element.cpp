bf86a47f181485b0910ade3044d0f406b329e077
Srajan Srivastava <srivasrajan8@gmail.com>
2026-01-31 03:21:42 +0530

    Kth Smallest Element in an array

--- HEAP/Leet215_Kth_Largest_Element.cpp
+++ HEAP/Leet215_Kth_Largest_Element.cpp
@@ -3,6 +3,7 @@
 #include <queue>
 using namespace std;
 
+// O(n log k) time complexity and O(k) space complexity
 int KthLargest(vector<int>& nums , int k)
 {
     priority_queue<int , vector<int> , greater<int>> pq;
@@ -25,12 +26,36 @@ int KthLargest(vector<int>& nums , int k)
     return ans;
 }
 
+// O(n log k) time complexity and O(k) space complexity
+int KthSmallest(vector<int>& nums , int k)
+{
+    priority_queue<int> pq;
+    for(int i = 0 ; i < k ; i++)
+    {
+        pq.push(nums[i]);
+    }
+
+    for(int i = k ; i <= nums.size() ; i++)
+    {
+        if(nums[i] < pq.top())
+        {
+            pq.pop();
+            pq.push(nums[i]);
+        }
+    }
+
+    int ans = pq.top();
+    return ans;
+}
+
 int main()
 {
     vector<int> arr = {3,2,1,5,6,4};
     int k = 2;
 
     cout<<"The Kth Largest Element is : "<<KthLargest(arr , k)<<endl;
+    cout<<endl<<endl;
+    cout<<"The Kth Smallest Element is : "<<KthSmallest(arr , k)<<endl;
 
     return 0;
 }
