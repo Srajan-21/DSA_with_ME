93a4a691a22cbad9a1c9000b9519b9c274bdd396
Srajan Srivastava <srivasrajan8@gmail.com>
2026-06-18 11:24:17 +0530

    Disjoint Set Union By Size Function

--- GRAPHS/Disjoint_Set.cpp
+++ GRAPHS/Disjoint_Set.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 class DisjointSet
 {
-    vector<int> rank,parent;
+    vector<int> rank,parent,size;
 
     public:
     DisjointSet(int n){
         rank.resize(n+1, 0);
         parent.resize(n+1);
+        size.resize(n+1);
 
         for(int i = 0 ; i <= n ; i++){
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -39,16 +41,33 @@ class DisjointSet
             rank[ulp_u]++;
         }
     }
+
+    void unionBySize(int u , int v){
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+
+        if(ulp_u == ulp_v)
+            return;
+
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
 };
 
 int main(){
 
     DisjointSet ds(7);
-    ds.unionByRank(1,2);
-    ds.unionByRank(2,3);
-    ds.unionByRank(4,5);
-    ds.unionByRank(6,7);
-    ds.unionByRank(5,6);
+    ds.unionBySize(1,2);
+    ds.unionBySize(2,3);
+    ds.unionBySize(4,5);
+    ds.unionBySize(6,7);
+    ds.unionBySize(5,6);
     
     // if 3 and 7 same or not
     if(ds.findUParent(3) == ds.findUParent(7))
@@ -56,7 +75,7 @@ int main(){
     else   
         cout<<"Not same"<<endl;
 
-    ds.unionByRank(3,7);
+    ds.unionBySize(3,7);
     if(ds.findUParent(3) == ds.findUParent(7))
         cout<<"Same"<<endl;
     else   
