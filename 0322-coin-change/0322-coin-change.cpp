7d08b19582366a5b9c6943b2d1a69d867ce26dcb
Srajan Srivastava <srivasrajan8@gmail.com>
2026-06-25 17:48:53 +0530

    Time: 19 ms (80.4%), Space: 17.8 MB (83.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -15,6 +15,7 @@ public:
         return mini;
     }
 
+    // Rec+memo
     int solveMemo(vector<int> &coins , int x , vector<int>&dp){
         if(x==0)return 0;
         if(x<0)return INT_MAX;
@@ -29,15 +30,33 @@ public:
         return mini;
     }
 
+    // Tabular
+    int solveTab(vector<int> &coins , int x){
+        vector<int> dp(x+1 , INT_MAX);
+        dp[0] = 0;
+        
+        for(int i = 1 ; i <= x ; i++){
+            for(int j = 0 ; j < coins.size() ; j++){
+                if(i-coins[j] >= 0 && dp[i-coins[j]] != INT_MAX){
+                    dp[i] = min(dp[i] , 1+dp[i-coins[j]]);
+                }
+            }
+        }
+        return dp[x] == INT_MAX ? -1 : dp[x];
+    }
+
     int coinChange(vector<int>& coins, int amount) {
         // TLE
         // int ans = solveRec(coins,amount);
         // if(ans == INT_MAX)return -1;
         // return ans;
 
-        //Rec + memo
-        vector<int> dp(amount+1 , -1);
-        int ans = solveMemo(coins,amount,dp);
-        return ans == INT_MAX ? -1 : ans;        
+        //Rec + memo TC - O(amount*n) SC-O(amount)
+        // vector<int> dp(amount+1 , -1);
+        // int ans = solveMemo(coins,amount,dp);
+        // return ans == INT_MAX ? -1 : ans;        
+
+        // Tab - TC - O(amount*n) SC-O(amount)
+        return solveTab(coins,amount);
     }
 };
