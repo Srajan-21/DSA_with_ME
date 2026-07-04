1e6abb5efe4ea34d0160e3873e95d8603704a202
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-04 19:21:45 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -26,6 +26,7 @@ public:
         return dp[n];
     }
 
+    // TC - O(n) SC - O(n)
     int solveTabular(vector<int>& nums){
         int n = nums.size();
         vector<int> dp(n,0);
@@ -39,17 +40,41 @@ public:
         return dp[n-1];
     }
 
+    // TC - O(n) SC - O(1)
+    int solveSpaceOptimization(vector<int>& nums){
+        int n = nums.size();
+
+        int prev2 = 0;
+        int prev1 = max(0,nums[0]);
+
+        for(int i = 1; i < n ; i++){
+            int inc = prev2 + nums[i];
+            int exc = prev1;
+
+            int ans = max(inc , exc);
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return prev1;
+    }
+
+
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,-1);
 
         long long ans = 0;
-        ans += solveTabular(nums);
+        ans += solveSpaceOptimization(nums);
         return ans;
+
+        // long long ans = 0;
+        // ans += solveTabular(nums);
+        // return ans;
 
         // long long ans = 0;
         // ans += solveRecursion(nums , n-1);
         // return ans;
+
         // return solveMemoization(nums,n-1,dp);
     }
 };
