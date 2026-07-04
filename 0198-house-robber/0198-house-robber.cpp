430a88c0ce51b6a50be0999606759187f82d9344
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-04 19:18:34 +0530

    Time: 0 ms (100%), Space: 10.6 MB (63.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
 
+    // TLE
+    int solveRecursion(vector<int>& nums , int n){
+
+        if(n < 0)return 0;
+
+        int including = solveRecursion(nums,n-2) + nums[n];
+        int excluding = solveRecursion(nums,n-1);
+
+        return max(including , excluding);
+        
+    }
+
     // TC - O(n) SC-O(n)
     int solveMemoization(vector<int>& nums , int n  , vector<int>&dp){
 
@@ -14,9 +26,30 @@ public:
         return dp[n];
     }
 
+    int solveTabular(vector<int>& nums){
+        int n = nums.size();
+        vector<int> dp(n,0);
+        dp[0] = max(0,nums[0]);
+
+        for(int i = 1 ; i < n ; i++){
+            int inc = (i>=2 ? dp[i-2] : 0) + nums[i];
+            int exc = dp[i-1];
+            dp[i] = max(inc,exc);
+        }
+        return dp[n-1];
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,-1);
-        return solveMemoization(nums,n-1,dp);
+
+        long long ans = 0;
+        ans += solveTabular(nums);
+        return ans;
+
+        // long long ans = 0;
+        // ans += solveRecursion(nums , n-1);
+        // return ans;
+        // return solveMemoization(nums,n-1,dp);
     }
 };
