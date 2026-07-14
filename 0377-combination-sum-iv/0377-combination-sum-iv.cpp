b6361239762531342c6881a83217927897055984
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-14 08:30:23 +0530

    Time: 0 ms (100%), Space: 9.1 MB (92.24%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -3,7 +3,20 @@ public:
 
     // Tabulation
     // TC - O(N × T) SC - O(T)
-    
+    int solveTABULATION(vector<int>& nums , int target){
+
+        vector<unsigned int> dp(target + 1 , 0);
+        dp[0] = 1;
+
+        for(int i = 1 ; i <= target ; i++){
+            for(int j = 0 ; j < nums.size() ; j++){
+                if(i >= nums[j]){
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
 
     // MEMOIZATION
     // TC - O(N × T)
@@ -48,8 +61,10 @@ public:
 
     int combinationSum4(vector<int>& nums, int target) {
 
-        vector<int>dp(target + 1 , -1);
-        return solveMEMOIZATION(nums , target , dp);
+        return solveTABULATION(nums , target);
+
+        // vector<int>dp(target + 1 , -1);
+        // return solveMEMOIZATION(nums , target , dp);
 
         // return solveRECURSION(nums , target);
         
