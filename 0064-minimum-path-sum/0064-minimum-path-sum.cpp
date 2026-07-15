bbeecdc9fcfe3744095c4f1726dbf70ed8738806
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-15 18:48:46 +0530

    Time: 3 ms (30.13%), Space: 16.2 MB (61.94%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -33,13 +33,48 @@ public:
         return dp[i][j] = grid[i][j] + min(right , down);
     }
 
+    // Time Complexity : O(m × n)
+    // Space Complexity : O(m × n)
+    int solveTABULATION(int m , int n , vector<vector<int>>& grid){
+
+        vector<vector<int>> dp(m , vector<int>(n , 0));
+
+        for(int i = m - 1 ; i >= 0 ; i--)
+        {
+            for(int j = n - 1 ; j >= 0 ; j--){
+
+                if(i == m - 1 && j == n - 1){
+                    dp[i][j] = grid[i][j];
+                }
+
+                else{
+                    int right = INF;
+                    int down = INF;
+
+                    if(i + 1 < m){
+                        right = dp[i + 1][j];
+                    }
+
+                    if(j + 1 < n){
+                        down = dp[i][j + 1];
+                    }
+
+                    dp[i][j] = grid[i][j] + min(right , down);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
 
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<vector<int>> dp(m , vector<int>(n , -1));
-        return solveMEMOIZATION(0 , 0 , m , n , grid , dp);
+        return solveTABULATION(m , n , grid);
+
+        // vector<vector<int>> dp(m , vector<int>(n , -1));
+        // return solveMEMOIZATION(0 , 0 , m , n , grid , dp);
 
         // return solveRECURSION(0 , 0 , m , n , grid);
 
