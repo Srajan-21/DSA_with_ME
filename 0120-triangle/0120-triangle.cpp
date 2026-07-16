ca141a41f198f422d27f41a792c152c50c39c8ae
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-16 21:11:14 +0530

    Time: 2 ms (16.61%), Space: 13.3 MB (59.23%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -34,11 +34,34 @@ public:
         return dp[i][j] = min(down , diagonal);
     }
 
+    // TC - O(m^2)
+    // SC - O(m^2) 
+    int solveTABULATION(vector<vector<int>>& triangle){
+        int m = triangle.size();
+        vector<vector<int>>dp(m , vector<int>(m , 0));
+
+        for(int j = 0 ; j < m ; j++){
+            dp[m-1][j] = triangle[m-1][j];
+        }
+
+        for(int i = m - 2 ; i >= 0 ; i--){
+            for(int j = i ; j >= 0 ; j--){
+                int down = triangle[i][j] + dp[i + 1][j];
+                int diagonal = triangle[i][j] + dp[i + 1][j + 1];
+                dp[i][j] = min(down , diagonal);
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
 
-        int m = triangle.size();
-        vector<vector<int>>dp(m , vector<int>(m , INF));
-        return solveMEMOIZATION(0 , 0 , triangle , dp);
+        return solveTABULATION(triangle);
+
+        // int m = triangle.size();
+        // vector<vector<int>>dp(m , vector<int>(m , INF));
+        // return solveMEMOIZATION(0 , 0 , triangle , dp);
 
         // return solveRECURSION(0 , 0 , triangle);
         
