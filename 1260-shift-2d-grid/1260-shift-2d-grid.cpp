526182f8d25f173efb47651d8b3b3b4ba97a7f63
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-20 06:08:03 +0530

    Time: 7 ms (19.5%), Space: 19.2 MB (17.4%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
 
         vector<int>temp;
-        for(int i = 0 ; i < grid.size() ; i++)
+        for(auto & row : grid)
         {
-            for(int j = 0 ; j < grid[0].size() ; j++)
+            for(auto& col : row)
             {
-                temp.push_back(grid[i][j]);
+                temp.push_back(col);
             }
         }
 
@@ -18,21 +18,23 @@ public:
         reverse(temp.begin() , temp.begin() + k);
         reverse(temp.begin() + k , temp.end());
 
-        vector<vector<int>> ans;
-        vector<int> t;
         int x = grid.size();
         int y = grid[0].size();
-        int m = y;
-        for(int i = 0 ; i < n ; i++)
+        int val = 0;
+        for(int i = 0 ; i < x ; i++)
         {
-           t.push_back(temp[i]); 
-            if(t.size() == y){
-                ans.push_back(t);
-                t.clear();
+            for(int j = 0 ; j < y ; j++)
+            {
+                grid[i][j] = temp[val++];
             }
+        //    t.push_back(temp[i]); 
+        //     if(t.size() == y){
+        //         ans.push_back(t);
+        //         t.clear();
+        //     }
         }
 
-        return ans;
+        return grid;
         
     }
 };
