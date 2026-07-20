d43b11fa10c3fd171961e7cafe33838da1eca74f
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-20 06:09:28 +0530

    Time: 0 ms (100%), Space: 19.4 MB (14.15%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
+
+    void reverse(vector<int> &nums,int start,int end){
+        for(int i=start,j=end;i<j;i++,j--){
+            swap(nums[i],nums[j]);
+        }
+    }
+
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
 
-        vector<int>temp;
+        vector<int>v;
         for(auto & row : grid)
         {
             for(auto& col : row)
             {
-                temp.push_back(col);
+                v.push_back(col);
             }
         }
 
-        int n = temp.size();
+        int n = v.size();
         k = k % n;
 
-        reverse(temp.begin() , temp.end());
-        reverse(temp.begin() , temp.begin() + k);
-        reverse(temp.begin() + k , temp.end());
+        reverse(v,0,v.size()-k-1);
+        reverse(v,v.size()-k,v.size()-1);
+        reverse(v,0,v.size()-1);
 
         int x = grid.size();
         int y = grid[0].size();
@@ -25,7 +32,7 @@ public:
         {
             for(int j = 0 ; j < y ; j++)
             {
-                grid[i][j] = temp[val++];
+                grid[i][j] = v[val++];
             }
         //    t.push_back(temp[i]); 
         //     if(t.size() == y){
