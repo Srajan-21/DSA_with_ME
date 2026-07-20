fa192c15fa24b44053c613051284c981a1d0ed8c
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-20 06:43:09 +0530

    Time: 23 ms (40.85%), Space: 43 MB (13.86%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
 
-    int solve(vector<int>& nums, int target , int i)
+    // TC - O(n×2001) ≈ O(n) (or O(n×sum))
+    // SC - O(n×2001)+O(n) ≈ O(n×sum)
+    int solveMEMOIZATION(vector<int>& nums, int target , int i , vector<vector<int>>& dp)
+    {
+        int n = nums.size();
+        if(target < -1000 || target > 1000)
+            return 0;
+
+        if(i == n)return target == 0;
+        int OFFSET = 1000;
+
+        if(dp[i][target + OFFSET] != -1)return dp[i][target + OFFSET];
+
+        int pos = solveMEMOIZATION(nums , target - nums[i] , i + 1 , dp);
+        int neg = solveMEMOIZATION(nums , target + nums[i] , i + 1 , dp);
+
+        return dp[i][target + OFFSET] = pos + neg;
+    }
+
+    // TC - O(2^n)
+    // SC - O(n)
+    int solveRECURSION(vector<int>& nums, int target , int i)
     {
         int n = nums.size();
         if(i == n)return target == 0;
 
-        int pos = solve(nums , target - nums[i] , i + 1);
-        int neg = solve(nums , target + nums[i] , i + 1);
+        int pos = solveRECURSION(nums , target - nums[i] , i + 1);
+        int neg = solveRECURSION(nums , target + nums[i] , i + 1);
 
         return pos + neg;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>>dp (n + 1 , vector<int>(2001 , -1));
+        return solveMEMOIZATION(nums , target , 0 , dp);
         
-        return solve(nums , target , 0);
+        // return solveRECURSION(nums , target , 0);
     }
 };
