b75c481752c0ef0d7f275f234701fc27eb57d645
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-21 00:26:23 +0530

    Time: 151 ms (35.83%), Space: 17.4 MB (65.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -40,6 +40,38 @@ public:
         return dp[i][target] = take || notTake;
     }
 
+    // Time Complexity: O(n * (totSum / 2)) ≈ O(n * totSum)
+    // Space Complexity: O(n * (totSum / 2)) ≈ O(n * totSum)
+    int solveTABULATION(vector<int>& nums , int target)
+    {
+        int n = nums.size();
+        vector<vector<bool>> dp(n , vector<bool>(target + 1 , 0));
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= target)
+            dp[0][nums[0]] = true;
+
+        for(int i = 1 ; i < n ; i++)
+        {
+            for(int j = 1 ; j <= target ; j++)
+            {
+                bool notTake = dp[i - 1][j];
+                bool take = false;
+                if(nums[i] <= j)
+                {
+                    take = dp[i - 1][j - nums[i]];
+                }
+                dp[i][j] = take || notTake;
+            }
+        }
+
+        return dp[n - 1][target];
+    }
+
     bool canPartition(vector<int>& nums) {
 
         int n = nums.size();
@@ -49,8 +81,10 @@ public:
 
         if(totSum % 2)return false;
 
-        vector<vector<int>> dp(n , vector<int>(totSum + 1 , -1));
-        return solveMEMOIZATION(nums , n - 1 , totSum / 2 , dp);
+        return solveTABULATION(nums , totSum / 2);
+
+        // vector<vector<int>> dp(n , vector<int>(totSum + 1 , -1));
+        // return solveMEMOIZATION(nums , n - 1 , totSum / 2 , dp);
 
         // return solveRECURSION(nums , n - 1 , totSum / 2);
 
