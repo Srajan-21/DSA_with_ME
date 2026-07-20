ccfe14cae2543e3b410c78444f1cb1dc7a0f50f9
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-20 07:11:15 +0530

    Time: 51 ms (36.94%), Space: 74 MB (5.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
 
     // TC - O(n×2001) ≈ O(n) (or O(n×sum))
+    // SC - O(n×2001) ≈ O(n×sum)
+    int solveTABULATION(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        int OFFSET = 1000;
+        vector<vector<int>>dp (n + 1 , vector<int>(2001 , 0));
+
+        dp[n][OFFSET] = 1;
+
+        for(int i = n - 1 ; i >= 0 ; i--)
+        {
+            for(int j = -1000 ; j <= 1000 ; j++)
+            {
+                int ways = 0;
+
+                if(j - nums[i] >= -1000 && j - nums[i] <= 1000)
+                    ways += dp[i + 1][j - nums[i] + OFFSET];
+
+                if(j + nums[i] >= -1000 && j + nums[i] <= 1000)
+                    ways += dp[i + 1][j + nums[i] + OFFSET];
+            
+                dp[i][j + OFFSET] = ways;
+            }
+        }
+
+        if(target < -1000 || target > 1000)
+        return 0;
+        
+        return dp[0][target + OFFSET];
+    }
+    
+    // TC - O(n×2001) ≈ O(n) (or O(n×sum))
     // SC - O(n×2001)+O(n) ≈ O(n×sum)
     int solveMEMOIZATION(vector<int>& nums, int target , int i , vector<vector<int>>& dp)
     {
@@ -36,8 +68,8 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         vector<vector<int>>dp (n + 1 , vector<int>(2001 , -1));
-        return solveMEMOIZATION(nums , target , 0 , dp);
-        
+        // return solveMEMOIZATION(nums , target , 0 , dp);
+        return solveTABULATION(nums , target);
         // return solveRECURSION(nums , target , 0);
     }
 };
