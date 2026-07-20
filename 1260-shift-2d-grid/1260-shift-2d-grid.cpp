8bc5f7187f7b0b845caacc7f9f986386eadc350a
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-20 06:11:55 +0530

    Time: 0 ms (100%), Space: 18.2 MB (39.39%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,47 +1,66 @@
 class Solution {
 public:
 
-    void reverse(vector<int> &nums,int start,int end){
-        for(int i=start,j=end;i<j;i++,j--){
-            swap(nums[i],nums[j]);
-        }
-    }
+    // void reverse(vector<int> &nums,int start,int end){
+    //     for(int i=start,j=end;i<j;i++,j--){
+    //         swap(nums[i],nums[j]);
+    //     }
+    // }
 
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
 
-        vector<int>v;
-        for(auto & row : grid)
-        {
-            for(auto& col : row)
-            {
-                v.push_back(col);
+        int m = grid.size();
+        int n = grid[0].size();
+        int total = m * n;
+
+        k %= total;
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int oldIndex = i * n + j;
+                int newIndex = (oldIndex + k) % total;
+
+                ans[newIndex / n][newIndex % n] = grid[i][j];
             }
         }
 
-        int n = v.size();
-        k = k % n;
-
-        reverse(v,0,v.size()-k-1);
-        reverse(v,v.size()-k,v.size()-1);
-        reverse(v,0,v.size()-1);
-
-        int x = grid.size();
-        int y = grid[0].size();
-        int val = 0;
-        for(int i = 0 ; i < x ; i++)
-        {
-            for(int j = 0 ; j < y ; j++)
-            {
-                grid[i][j] = v[val++];
-            }
-        //    t.push_back(temp[i]); 
-        //     if(t.size() == y){
-        //         ans.push_back(t);
-        //         t.clear();
+        return ans;
+        // vector<int>v;
+        // for(auto & row : grid)
+        // {
+        //     for(auto& col : row)
+        //     {
+        //         v.push_back(col);
         //     }
-        }
+        // }
 
-        return grid;
+        // int n = v.size();
+        // k = k % n;
+
+        // reverse(v,0,v.size()-k-1);
+        // reverse(v,v.size()-k,v.size()-1);
+        // reverse(v,0,v.size()-1);
+
+        // int x = grid.size();
+        // int y = grid[0].size();
+        // int val = 0;
+        // for(int i = 0 ; i < x ; i++)
+        // {
+        //     for(int j = 0 ; j < y ; j++)
+        //     {
+        //         grid[i][j] = v[val++];
+        //     }
+        // //    t.push_back(temp[i]); 
+        // //     if(t.size() == y){
+        // //         ans.push_back(t);
+        // //         t.clear();
+        // //     }
+        // }
+
+        // return grid;
         
     }
 };
