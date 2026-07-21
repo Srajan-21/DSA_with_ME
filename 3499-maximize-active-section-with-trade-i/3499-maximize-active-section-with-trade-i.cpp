962ac06306b10f556fc12180c6bf999cc5341ef1
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-21 15:13:44 +0530

    Time: 78 ms (47.3%), Space: 54.8 MB (90.54%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -9,25 +9,54 @@ public:
             ones++;
         }
 
-        vector<pair<char , int>> runs;
-        for(int i = 0 ; i < n ;)
-        {
+        int ans = ones;
+
+        int len1 = 0, len2 = 0, len3 = 0;
+        char ch1 = '#', ch2 = '#', ch3 = '#';
+
+        int i = 0;
+        while (i < n) {
             int j = i;
-            while(j < n && s[j] == s[i])
+            while (j < n && s[j] == s[i])
                 j++;
 
-            runs.push_back({s[i] , j - i});
+            // Current run
+            ch1 = ch2;
+            len1 = len2;
+
+            ch2 = ch3;
+            len2 = len3;
+
+            ch3 = s[i];
+            len3 = j - i;
+
+            // Check pattern: 0-run, 1-run, 0-run
+            if (ch1 == '0' && ch2 == '1' && ch3 == '0') {
+                ans = max(ans, ones + len1 + len3);
+            }
+
             i = j;
         }
 
-        int ans = ones;
-        for(int i = 1 ; i < runs.size() - 1 ; i++)
-        {
-            if(runs[i].first == '1' && runs[i - 1].first == '0' && runs[i + 1].first == '0')
-            {
-                ans = max(ans , ones + runs[i - 1].second + runs[i + 1].second);
-            }
-        }
+        // vector<pair<char , int>> runs;
+        // for(int i = 0 ; i < n ;)
+        // {
+        //     int j = i;
+        //     while(j < n && s[j] == s[i])
+        //         j++;
+
+        //     runs.push_back({s[i] , j - i});
+        //     i = j;
+        // }
+
+        // int ans = ones;
+        // for(int i = 1 ; i < runs.size() - 1 ; i++)
+        // {
+        //     if(runs[i].first == '1' && runs[i - 1].first == '0' && runs[i + 1].first == '0')
+        //     {
+        //         ans = max(ans , ones + runs[i - 1].second + runs[i + 1].second);
+        //     }
+        // }
 
         return ans;
 
