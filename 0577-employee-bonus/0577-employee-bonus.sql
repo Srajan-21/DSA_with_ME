ad259505687afd15b8670c06f5b50590a21cb058
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-26 01:41:24 +0530

    Time: 1083 ms (37.27%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,5 @@
-select name , bonus
-from Employee
-left join Bonus 
-on Employee.empId = Bonus.empId
-where bonus < 1000 or bonus is null; 
+select e.name , b.bonus
+from Employee e
+left join Bonus b 
+on e.empId = b.empId
+where b.bonus < 1000 or b.bonus is null; 
