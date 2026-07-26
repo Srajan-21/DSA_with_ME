99e03038ca2a1ebf3e25e82d5709e7198de09347
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-26 07:29:02 +0530

    Time: 774 ms (11.09%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,22 @@
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    t.country,
+    (
+        COUNT(t.state)
+    ) AS trans_count,
+    (
+        SUM(t.state = 'approved')
+    ) AS approved_count,
+    (
+        SUM(t.amount)
+    ) AS trans_total_amount,
+    (
+        SUM(CASE WHEN t.state = 'approved' THEN t.amount ELSE 0 END)
+        -- SUM(IF(t.state = 'approved' , t.amount , 0))
+        -- SUM(t.amount WHERE t.state = 'approved') THIS IS NOT VALID SYNTAX
+    ) AS approved_total_amount
+FROM 
+    Transactions t
+GROUP BY 
+    DATE_FORMAT(trans_date, '%Y-%m'),
+    country;
