88a8fea8b2ee96d04f31d0660834a743b966a562
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-27 17:58:58 +0530

    Time: 585 ms (46.24%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,13 @@
+SELECT
+    e.employee_id,
+    e.department_id
+FROM
+    Employee e
+WHERE   
+    primary_flag = 'Y'
+    OR employee_id IN (
+        SELECT employee_id
+        FROM Employee
+        GROUP BY employee_id
+        HAVING COUNT(*) = 1
+    );
