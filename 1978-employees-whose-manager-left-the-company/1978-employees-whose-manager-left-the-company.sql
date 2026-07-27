0ef1681df07217d8a371ded9d321fe26a1fd32ec
Srajan Srivastava <srivasrajan8@gmail.com>
2026-07-27 22:40:15 +0530

    Time: 527 ms (6.47%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,11 +1,21 @@
-select e.employee_id
-from (
-    select *
-    from Employees
-    where salary < 30000
-) e
-where e.manager_id not in (
-    select employee_id
-    from Employees
-)
+select employee_id
+from Employees
+where salary < 30000
+    and manager_id is not null
+    and manager_id not in(
+        select employee_id
+        from Employees
+    )
 order by employee_id;
+
+-- select e.employee_id
+-- from (
+--     select *
+--     from Employees
+--     where salary < 30000
+-- ) e
+-- where e.manager_id not in (
+--     select employee_id
+--     from Employees
+-- )
+-- order by employee_id;
